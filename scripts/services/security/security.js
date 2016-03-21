815a3610fc9166c3cf703d781d50e326245d68ea
wei.he <15030774@cnsuning.com>
2016-03-21 09:45:55 +0800

    add security module

--- scripts/services/security/security.js
+++ scripts/services/security/security.js
@@ -1,30 +1,38 @@
-angular.module('app.security', []).factory('Security', function(securityQueue) {
+angular.module('app.security', []).factory('Security', function($q, securityQueue) {
   'use strict';
+
   var result = {
     showDialog: function() {
-
+      console.log('this is dialog!');
+      this.login().then(function(v) {
+        securityQueue.retryAll();
+      });
     },
     cancelDialog: function() {
-
+      securityQueue.retryCancel();
     },
     login: function() {
-
+      return $q.when('login');
     }
   };
 
+  securityQueue.onItemAdd = function() {
+    result.showDialog();
+  };
+
   return result;
-}).factory('SecurityInterceptor', ['SecurityQueue', function($q, queue) {
+}).factory('SecurityInterceptor', ['SecurityQueue', function($q, $http, queue) {
   'use strict';
   return {
     responseError: function(response) {
       if (response.status === 401) {
-        return queue.pushRetryItem('Unauthorized', function(){
-        })
+        return queue.pushRetryItem('Unauthorized', function() {
+          return $http(response.config);
+        });
       }
       return response;
     }
-  }
-
+  };
 }]).service('SecurityQueue', function($q) {
   'use strict';
   var retryItems = [];
@@ -33,7 +41,7 @@ angular.module('app.security', []).factory('Security', function(securityQueue) {
     this.onItemAdd();
   }.bind(this);
 
-  //需使用的地方实现相应内容 
+  //abstract method需使用的地方实现相应内容 
   this.onItemAdd = function() {
     throw new Error('onItemadd function is unimplements');
   };
@@ -57,6 +65,18 @@ angular.module('app.security', []).factory('Security', function(securityQueue) {
     push(retryItem);
 
     return defer.promise;
+  };
+
+  this.retryAll = function() {
+    retryItems.forEach(function(v) {
+      v.retry();
+    });
+  };
+
+  this.retryCancel = function() {
+    retryItems.forEach(function(v) {
+      v.cancel();
+    });
   };
 
   this.hasMore = function() {
